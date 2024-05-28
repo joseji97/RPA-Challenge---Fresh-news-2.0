4450446f32311a59112e3f9cd3235afcb62250fa
joseji97 <jjimenez@fiuna.edu.py>
2024-05-28 08:16:14 -0400

    typo

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from utils import Utils
 
-class WebScrapper(Utils):
+class WebScraper(Utils):
     def _init_(self):
         super().__init__()
 
 if __name__ == '__main__':
-    WebScrapperBot = WebScrapper()
-    WebScrapperBot.open_website()
-    WebScrapperBot.begin_search()
-    WebScrapperBot.select_topic()
-    WebScrapperBot.sort_newest_news()
-    WebScrapperBot.set_month_range()
-    WebScrapperBot.extract_website_data()
-    WebScrapperBot.close_browser()
-    WebScrapperBot.print_and_log("info","Bot execution finished")
+    WebScraperBot = WebScraper()
+    WebScraperBot.open_website()
+    WebScraperBot.begin_search()
+    WebScraperBot.select_topic()
+    WebScraperBot.sort_newest_news()
+    WebScraperBot.set_month_range()
+    WebScraperBot.extract_website_data()
+    WebScraperBot.close_browser()
+    WebScraperBot.print_and_log("info","Bot execution finished")
